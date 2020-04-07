3b1dd830ba138a5811321792c821ce65609dc38a
Zach Huffman <huffmanz2012@gmail.com>
2020-04-07 10:23:14 -0500

    bad state tracking example

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,6 +18,10 @@ public class PlayerController : MonoBehaviour
 
     private SpriteRenderer face;
     private Rigidbody rbody;
+    private bool isJumping = false;
+    private bool isDucking = false;
+    private bool isSpinning = false;
+    private float rotation;
 
     #endregion
     
@@ -31,7 +35,59 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if(Input.GetButtonDown("Jump")){
+           if(!isJumping && !isDucking){
+            SetExpression(jumpingSprite) ;
+            rbody.AddForce(Vector3.up * jumpForce);
+           }
+           isJumping = true;
+        }  
+        else if(Input.GetButtonDown("Duck")){
+            if(!isJumping){
+                isDucking=true;
+                head.localPosition = new Vector3(head.localPosition.x, .5f, head.localPosition.z);
+                SetExpression(duckingSprite);
+            }else{
+                Spin();
+                isSpinning = true;
+                SetExpression(spinningSprite);
+            }
+        }    
+        else if(Input.GetButtonUp("Duck")) {
+            if(!isJumping){
+                isDucking =false;
+                head.localPosition = new Vector3(head.localPosition.x, .8f, head.localPosition.z);
+                SetExpression(idleSprite);
+            }
+        }else if(Input.GetButtonDown("SwapWeapon")){
+            if(!isJumping && !isSpinning && !isDucking){
+                bool usingWeapon01 = weapon01.gameObject.activeInHierarchy;
+                weapon01.gameObject.SetActive(!usingWeapon01);
+                weapon02.gameObject.SetActive(usingWeapon01);
+            }
+        }
+        if(isSpinning){
+            Spin();
+        }
+    }
+
+    private void Spin(){
+        float amountToRotate = 900 * Time.deltaTime;
+        rotation+=amountToRotate;
+        if(rotation < 360f){
+            transform.Rotate(Vector3.up, amountToRotate);
+        } else {
+            transform.rotation = Quaternion.identity;
+            isSpinning = false;
+            rotation = 0;
+            SetExpression(jumpingSprite);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        isJumping = false;
+        SetExpression(idleSprite);
     }
 
     public void SetExpression(Sprite newExpression)
