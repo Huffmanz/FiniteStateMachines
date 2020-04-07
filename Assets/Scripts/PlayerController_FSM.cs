4143dd9a617a877e67d6227d2ac43d621ba64694
Zach Huffman <huffmanz2012@gmail.com>
2020-04-07 14:10:37 -0500

    finite state machine /  better state management

--- Assets/Scripts/PlayerController_FSM.cs
+++ Assets/Scripts/PlayerController_FSM.cs
@@ -11,6 +11,11 @@ public class PlayerController_FSM : MonoBehaviour
     public Transform weapon01;
     public Transform weapon02;
 
+
+    public readonly PlayerIdleState idleState = new PlayerIdleState();
+    public readonly PlayerJumpingState jumpingState = new PlayerJumpingState();
+    public readonly PlayerDuckingState duckingState = new PlayerDuckingState();
+
     public Sprite idleSprite;
     public Sprite duckingSprite;
     public Sprite jumpingSprite;
@@ -18,8 +23,16 @@ public class PlayerController_FSM : MonoBehaviour
 
     private SpriteRenderer face;
     private Rigidbody rbody;
+    public Rigidbody Rigidbody{
+        get {return rbody;}
+    }
+    private PlayerBaseState currentState;
+    public PlayerBaseState CurrentState{
+        get {return currentState;}
+    }
 
     #endregion
+
 
     private void Awake()
     {
@@ -28,12 +41,28 @@ public class PlayerController_FSM : MonoBehaviour
         SetExpression(idleSprite);
     }
 
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        TransitionToState(idleState);
+    }
     // Update is called once per frame
     void Update()
     {
-
+        currentState.Update(this);
     }
 
+    void OnCollisionEnter(Collision other)
+    {
+        currentState.OnCollisionEnter(this);
+    }
+    public void TransitionToState(PlayerBaseState state){
+        currentState = state;
+        currentState.EnterState(this);
+    }
     public void SetExpression(Sprite newExpression)
     {
         face.sprite = newExpression;
